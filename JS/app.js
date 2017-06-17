16dbe86b04742c8a9cd46a58ceb930d2d471769b
Steven Rayl <stevenmrayl@gmail.com>
2017-06-17 08:16:37 -0700

    More authorization fixes

--- JS/app.js
+++ JS/app.js
@@ -1,48 +1,60 @@
 var APIKey = "62f422f9869847708f9524db355258ef";
 var clientID = 13464;
-var authURL = "https://www.bungie.net/en/OAuth/Authorize?client_id=" + clientID;
+var authURL = "https://www.bungie.net/en/OAuth/Authorize?response_type=code&client_id=" + clientID;
 var tokenURL = "https://www.bungie.net/Platform/App/GetAccessTokensFromCode/";
 var accessTokenExpires;
 var APIScope;
+var urlParams;
+
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
 
 function authorize () {
 	localStorage.setItem("BungieAPIState", "" + btoa(Math.random(new Date().getTime())));
 	console.log(localStorage.getItem("BungieAPIState"));
-	$.ajax({
-		url: authURL,
-		method: "POST",
-		headers: {
-			"X-API-Key": APIKey,
-			"state": localStorage.getItem("BungieAPIState")
-		}
-	}).done(function(json){
-		console.log(json.Response);
-		var code = json.Response.data.code;
-		var state = json.Response.data.state;
-		if (state == localStorage.getItem("BungieAPIState"))
-		{
-			$.ajax({
-				method: "POST",
-				url: tokenURL,
-				headers: {
-					"X-API-Key": APIKey,
-				},
-				data: {
-					"code": code,
-				}
-			}).done(function(json){
-				console.log(json.Response);
-				localStorage.setItem("accessToken", json.Response.accessToken.value);
-				localStorage.setItem("refreshToken", json.Response.refreshToken.value);
-				localStorage.setItem("accessTokenExpires", json.Response.accessToken.expires);
-				//Make sure to check that the scope is correct for the api access we need to function.
-				APIScope = json.Response.scope;
-			});
-		}			
-		console.log(json.Response.headers.state);
-	});
+	window.location = authURL +"&state=" + localStorage.getItem("BungieAPIState");
+}
+
+function postAuthorize ()
+{
+	$("#authorize-label").text = "Authorizing...";
+	if (urlParams["state"] == localStorage.getItem("BungieAPIState"))
+	{
+		$.ajax({
+			method: "POST",
+			url: tokenURL,
+			headers: {
+				"X-API-Key": APIKey,
+			},
+			data: {
+				"code": urlParams["code"],
+			}
+		}).done(function(json){
+			console.log(json.Response);
+			localStorage.setItem("accessToken", json.Response.accessToken.value);
+			localStorage.setItem("refreshToken", json.Response.refreshToken.value);
+			localStorage.setItem("accessTokenExpires", json.Response.accessToken.expires);
+			//Make sure to check that the scope is correct for the api access we need to function.
+			APIScope = json.Response.scope;
+			$("#authorize-label").text = "Authorization complete";
+		});
+	}			
 }
 
 $(function(){
+	if(urlParams["code"] && urlParams["state"] && urlParams["state"] == localStorage.getItem("BungieAPIState"))
+	{
+		console.log("post authorize");
+		postAuthorize ();
+	}
 	$("#authorize-button").click(function(){authorize();});
 });
